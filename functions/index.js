9061b8b67a75ada397aba32a5d468ac1eb3da05c
Gurneer Bedi <gurneer1223@gmail.com>
2025-03-13 14:01:51 -0400

    deployment changes

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-import photosRouter from "./routes/photos.js";
-import tagsRouter from "./routes/tags.js";
+import photosRouter from "../routes/photos.js";
+import tagsRouter from "../routes/tags.js";
+
 
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
+const serverless = require("serverless-http");
 
 app.use("/photos", photosRouter);
 app.use("/tags", tagsRouter);
@@ -14,3 +16,5 @@ app.use("/tags", tagsRouter);
 app.listen(process.env.PORT || 8080, () =>
     console.log(`Express server listening on port ${process.env.PORT || 8080}`)
 );
+
+module.exports.handler = serverless(app);
